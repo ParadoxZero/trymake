c725bd7b235cad69c79ea3b2bce86f3b1e981f9e
sidhin <sidhin@trymake.com>
2017-07-09 03:23:45 +0530

    Twitter login working.

--- trymake/settings.py
+++ trymake/settings.py
@@ -159,11 +159,26 @@ CSRF_USE_SESSIONS = True
 
 # Social Django
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
 
 SOCIAL_AUTH_GITHUB_KEY = '41af3b1d2a8be6edfd13'
 SOCIAL_AUTH_GITHUB_SECRET = 'e984cf00bf50094f6a3eee0de8d6c43bf9347bd7'
 SOCIAL_AUTH_GITHUB_SCOPE = ['user']
+
+SOCIAL_AUTH_TWITTER_KEY = '8I38YrIxBjv0i3E9stm5fArvG'
+SOCIAL_AUTH_TWITTER_SECRET = 'HW7NFUuSfNyF7ULQsV5MFetIS4lML9steFvA0LhFccTVHcq5po'
 
 # Custom settings
 
